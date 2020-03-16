26c58341bc9f0e58236294dc2378983ec2e20a19
王玉阳 <epis2048@outlook.com>
2020-03-16 17:24:24 +0800

    æéç³»ç»å®æ

---
+++ auth.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <ctime>
+
+typedef struct admin{
+	int id;
+	char username[20];//用户名，登陆用
+	char realname[100];//真实姓名，显示用
+	char password[50];//密码，后期以md5存储
+	time_t recentLoginTime;//最近登陆时间，存储为时间戳
+	time_t createTime;//账户创建时间，存储为时间戳
+	int authList[6] = { 0, 0, 0, 0, 0, 0 };//权限列表
+	struct admin* next = NULL;
+} adminList;
+/*
+AuthList说明
+0 超级管理员
+1 上下机
+2 充值退费
+3 卡管理
+4 计费标准管理
+5 日志查询
+*/
+
+//输入时隐藏密码
+void inputPassword(char password[50], int size);
+//获取登陆状态
+int auth_checkStatus(adminList* head_adminList, int nowAdminID);
+//登录
+int auth_login(adminList* head_adminList);
+//检查是否有某项的权限
+int auth_checkHasAuth(adminList* head_adminList, int nowAdminID, int authID);
+
+//显示权限管理主菜单
+adminList* auth_showMainMenu(adminList* head_adminList, int nowAdminID);
+//查询管理员
+int auth_queryAdminDetail(adminList* head_adminList);
+//添加管理员
+adminList* auth_do_addAdmin(adminList* head_adminList, char username[20], char realname[100], char password[50], int isSuperAdmin, int nowAdminID);
+adminList* auth_addAdmin(adminList* head_adminList,  int nowAdminID);
+//编辑管理员
+adminList* auth_editAdmin(adminList* head_adminList, int nowAdminID);
+//删除管理员
+adminList* auth_do_delAdmin(adminList* head_adminList, int delID, int nowAdminID);
+adminList* auth_delAdmin(adminList* head_adminList, int nowAdminID);
+
+
---
+++ main.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+#include "menu.h"
+#include "auth.h"
+
+
+int main()
+{
+	/*
+	初始化，先用链表，后期读取配置文件
+	*/
+	adminList* head_adminList = NULL;
+	int nowAdminID = 0;
+	//初始化admin/123456的用户
+	char in_al_un[20] = "admin";
+	char in_al_rn[1000] = "Admin";
+	char in_al_pw[50] = "asdfghjkl;'";
+	head_adminList = auth_do_addAdmin(head_adminList, in_al_un, in_al_rn, in_al_pw, 1, 0);
+	int userChoose = 0;
+	while (1) {
+		if (auth_checkStatus(head_adminList, nowAdminID) != 0) {
+			userChoose = showMenu(head_adminList, nowAdminID);
+			switch (userChoose){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				if (auth_checkHasAuth(head_adminList, nowAdminID, 0)) head_adminList = auth_showMainMenu(head_adminList, nowAdminID);
+				system("cls");
+				break;
+			case 9:
+				break;
+			case 0:
+				break;
+			default:
+				cout << "请选择正确的选项！" << endl;
+				system("pause");
+				system("cls");
+				break;
+			}
+			if (userChoose == 0) {
+				system("cls");
+				nowAdminID = 0;
+			}
+		}
+		else {
+			nowAdminID = auth_login(head_adminList);
+			if (nowAdminID < 0) break;
+		}
+	}
+	return 0;
+}
